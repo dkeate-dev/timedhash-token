2ac7c5fcf7e4f4d5816531411f25a31607dc91f8
Dustin Keate <dkeate-dev@gmail.com>
2023-02-05 22:42:26 +0000

    Add basic tree functionality

--- .test_tree.py
+++ .test_tree.py
@@ -1,7 +1,22 @@
-def append_2(a_list):
-    a_list.append(2)
+from time import sleep
+
+from transaction import Transaction
+from tree import HeadNode
 
 if __name__ == "__main__":
-    my_list = [1]
-    append_2(my_list)
-    print(my_list)
+    t2 = Transaction()
+    sleep(.1)
+    t1 = Transaction()
+    sleep(.1)
+    t3 = Transaction()
+    sleep(.1)
+    t4 = Transaction()
+
+    my_head_node = HeadNode([t1, t2, t3, t4])
+
+    print(my_head_node)
+
+    transaction_list = my_head_node.get_transaction_list()
+
+    for t in transaction_list:
+        print(t)
